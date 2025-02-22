9398aa226d4b679a172de491e4ec1e66a7fd5e3b
Joel Denning <joeldenning@gmail.com>
2025-02-21 23:06:49 -0700

    Fixes

---
+++ src/main.ts
@@ -0,0 +1,25 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { ApplicationRef } from '@angular/core';
+import { chooseDomElementGetter } from 'dom-element-getter-helpers'
+
+let appRef: ApplicationRef
+
+export async function bootstrap() {
+  console.log('bootstrapping');
+}
+
+export async function mount(props: any) {
+  console.log('mounting', props);
+  const domElement = chooseDomElementGetter({}, props)();
+  const rootElement = document.createElement('rate-doggos-root')
+  domElement.appendChild(rootElement);
+  appRef = await bootstrapApplication(AppComponent, appConfig)
+  return appRef;
+}
+
+export async function unmount() {
+  console.log('unmounting');
+  appRef.destroy();
+}
